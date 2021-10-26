32315a7f5e234463c80aac70221cec210801e2a2
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-10-26 15:18:21 -0500

    Thibg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestController.java
@@ -1,26 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name="Linear Op Mode test", group="Linear Opmode")
-public class TestController extends LinearOpMode {
+public class TestController extends OpMode {
 
     private ElapsedTime runtime;
-    private DcMotor dcMotor;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
+    public void init() {
         runtime = new ElapsedTime();
-        while (opModeIsActive()){
-            //Code body
-            if (gamepad1.x){
-                System.out.println("Test");
-                System.out.println("Elapsed time: " + runtime.time());
-            }
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.touchpad){
+            telemetry.addData("Elapsed time (when key pressed): ", runtime.time());
+            telemetry.update();
         }
     }
 }
