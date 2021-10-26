14c62a3fcb73c25b7110aaba9ac844dc82a8d034
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-10-26 15:48:37 -0500

    Things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestController.java
@@ -4,27 +4,68 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name="Linear Op Mode test", group="Linear Opmode")
 public class TestController extends OpMode {
+    //Note this code did not directly come from the examples but is heavily inspired by the examples
+    //along with the code from last year, I have made some changes but a decent amount of the logic
+    //is paraphrased, this is merely just a foundation that we will work to change later.
 
     private ElapsedTime runtime;
+    private DcMotor frontRight;
+    private DcMotor frontLeft;
+    private DcMotor backRight;
+    private DcMotor backLeft;
 
     @Override
     public void init() {
+        //Starts the operation mode
         runtime = new ElapsedTime();
         telemetry.addData("Status", "Started");
         telemetry.update();
+
+        //Assuming that there are four drive wheels
+        frontRight  = hardwareMap.get(DcMotor.class, "left_drive");
+        frontLeft = hardwareMap.get(DcMotor.class, "right_drive");
+        backRight = hardwareMap.get(DcMotor.class, "left_drive");
+        backLeft = hardwareMap.get(DcMotor.class, "right_drive");
+
+        //Setting the motors to stop when there is no power
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Just have raw power input
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //Setting the right side to reverse (it is what the example code and last year's code does,
+        // it apparently makes the robot go forwards, do not question it)
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.touchpad){
-            telemetry.addData("Elapsed time (when key pressed): ", runtime.time());
-            telemetry.update();
-        }
+        //Here you will have to make the main logic of the operation mode
+        //Ok apparently when you have the stick forwards it is negative y so you have to negate it
+        double drivePower = -gamepad1.left_stick_y;
+        double turnPower = gamepad1.right_stick_x;
+
+        //So this part makes sure that the power is not too high or too low, it also makes the robot
+        //actually move
+        frontRight.setPower(Range.clip(drivePower - turnPower, -1, 1));
+        backRight.setPower(Range.clip(drivePower - turnPower, -1, 1));
+
+        frontLeft.setPower(Range.clip(drivePower + turnPower, -1, 1));
+        backLeft.setPower(Range.clip(drivePower + turnPower, -1, 1));
     }
 }
