3f920c8648457f0248c8f863ab5c4bd7e3597f12
thatdanield <82678093+thatdanield@users.noreply.github.com>
2021-11-13 10:00:02 -0600

    Edited lift functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automodes/AutoTest.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.automodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RoboOp;
+
+@Autonomous(name="AutoTest", group="Auto")
+public class AutoTest extends RoboOp {
+
+    public final double speed = 0.69;
+    public double timePassed;
+    public boolean one;
+    public double timer;
+    public double offset;
+    public static final double TILE_SIZE = 0.6096;
+    public boolean clawSetup;
+    public boolean claw;
+
+    @Override
+    public void init() {
+        super.init();
+        timer = runtime.time();
+        offset = 0;
+        drivePower = 0;
+        clawSetup = false;
+        one = false;
+        claw = false;
+    }
+
+    @Override
+    public void loop() {
+        super.loop();
+
+        timePassed = runtime.time() - offset;
+
+        //向前走
+        if (!clawSetup) {
+            if (timePassed > 3 && leftClaw.getPosition()==0.45 && rightClaw.getPosition()==0.45) {
+                offset = timePassed;
+                clawSetup = true;
+            } else if (timePassed > 2 && leftClaw.getPosition()!=0.45) {
+                leftClaw.setPosition(0.45);
+            } else if (rightClaw.getPosition()!=0.45){
+                rightClaw.setPosition(0.45);
+            }
+
+            return;
+        }
+        if (!one) {
+            drivePower = 0.5;
+            if ((timePassed * speed) > 1.5*TILE_SIZE) {
+                drivePower = 0;
+                offset = runtime.time();
+                one = true;
+            }
+            return;
+        }
+
+    }
+}
