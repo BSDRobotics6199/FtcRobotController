0a6e8339040d4d7d010f44479c0c58dc6d08d552
thatdanield <82678093+thatdanield@users.noreply.github.com>
2021-11-19 13:36:47 -0600

    things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Measure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Measure.java
@@ -10,13 +10,21 @@ public class Measure extends RoboOp {
     @Override
     public void init() {
         super.init();
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.loggingEnabled = false;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
     }
     double offsetX = 0;
     double offsetY = 0;
     @Override
     public void loop() {
         dt = runtime.time() - lastTime;
-        telemetry.addData("DeltaTime", dt);
+        telemetry.addData("IMU data", imu.getPosition().x + " " + imu.getPosition().y + " " +
+                imu.getPosition().z);
 
         //在这里设能量
         drivePower = -gamepad1.left_stick_y;
