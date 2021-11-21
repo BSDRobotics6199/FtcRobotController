8dd553548b37978df713cf2eb8c871321827d82b
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-11-20 22:11:12 -0600

    自动模式改成roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automodes/AutoB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automodes/AutoB.java
@@ -1,14 +1,19 @@
 package org.firstinspires.ftc.teamcode.automodes;
 
+import android.hardware.camera2.params.MeteringRectangle;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.sun.tools.javac.tree.JCTree;
 
 import org.firstinspires.ftc.teamcode.RoboOp;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 import java.util.ArrayList;
 
@@ -19,6 +24,13 @@ public class AutoB extends RoboOp {
 
     public static final double METER_PER_INCH = 0.0254;
     public static final double METER_PER_TILE = 0.6096;
+
+    public enum Direction {
+        FORWARD,
+        LEFT,
+        RIGHT,
+        BACK
+    }
 
     private boolean tasks;
 
@@ -56,5 +68,25 @@ public class AutoB extends RoboOp {
         drive.followTrajectory(
                 drive.trajectoryBuilder(drive.getPoseEstimate()).back((METER_PER_TILE/2)/METER_PER_INCH).build()
         );
+    }
+
+    public void move(double tiles, Direction direction){
+        TrajectoryBuilder builder = drive.trajectoryBuilder(drive.getPoseEstimate());
+        switch (direction){
+            case FORWARD:
+                builder.forward(tiles*METER_PER_TILE/METER_PER_TILE);
+                break;
+            case LEFT:
+                builder.strafeLeft(tiles*METER_PER_TILE/METER_PER_TILE);
+                break;
+            case RIGHT:
+                builder.strafeRight(tiles*METER_PER_TILE/METER_PER_TILE);
+                break;
+            case BACK:
+                builder.back(tiles*METER_PER_TILE/METER_PER_TILE);
+                break;
+
+        }
+        drive.followTrajectory(builder.build());
     }
 }
