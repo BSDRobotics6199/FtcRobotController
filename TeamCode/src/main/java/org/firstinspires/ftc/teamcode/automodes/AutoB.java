617711115985bb06be859b2bf1c934127b3ef6a3
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2021-12-08 12:34:02 -0600

    修了程序

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automodes/AutoB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/automodes/AutoB.java
@@ -74,16 +74,16 @@ public class AutoB extends RoboOp {
         TrajectoryBuilder builder = drive.trajectoryBuilder(drive.getPoseEstimate());
         switch (direction){
             case FORWARD:
-                builder.forward(tiles*METER_PER_TILE/METER_PER_TILE);
+                builder.forward(tiles*METER_PER_TILE/METER_PER_INCH);
                 break;
             case LEFT:
-                builder.strafeLeft(tiles*METER_PER_TILE/METER_PER_TILE);
+                builder.strafeLeft(tiles*METER_PER_TILE/METER_PER_INCH);
                 break;
             case RIGHT:
-                builder.strafeRight(tiles*METER_PER_TILE/METER_PER_TILE);
+                builder.strafeRight(tiles*METER_PER_TILE/METER_PER_INCH);
                 break;
             case BACK:
-                builder.back(tiles*METER_PER_TILE/METER_PER_TILE);
+                builder.back(tiles*METER_PER_TILE/METER_PER_INCH);
                 break;
 
         }
